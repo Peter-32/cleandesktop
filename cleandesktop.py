b8925b770ed0cad455523030ac220260fc39c980
Peter-32 <peter@impactradius.com>
2018-09-01 11:23:21 -0700

    a

--- cleandesktop.py
+++ cleandesktop.py
@@ -2,20 +2,20 @@ import os
 
 # Inputs
 desktop_directory = "/Users/peterjmyers/Desktop"
-do_not_clean_files_and_folders = ['Documents', 'Temp', 'Inbox', 'Trash', '.DS_Store']
+do_not_clean_files_and_folders = ['To-do', 'Janitor', 'Code', 'Other', '.DS_Store']
 
-def create_temp_directory_if_not_exists():
-    full_directory_to_temp = "{}/Temp".format(desktop_directory)
-    if not os.path.exists(full_directory_to_temp):
-        os.makedirs(full_directory_to_temp)
+def create_janitor_directory_if_not_exists():
+    full_directory_to_janitor = "{}/Janitor".format(desktop_directory)
+    if not os.path.exists(full_directory_to_janitor):
+        os.makedirs(full_directory_to_janitor)
 
-def move_files_to_temp():
+def move_files_to_janitor():
     documents = os.listdir(desktop_directory)
     for doc in documents:
         if doc not in do_not_clean_files_and_folders:
-            os.rename("{}/{}".format(desktop_directory, doc), "{}/Temp/{}".format(desktop_directory, doc))
+            os.rename("{}/{}".format(desktop_directory, doc), "{}/Janitor/{}".format(desktop_directory, doc))
 
 if __name__ == "__main__":
-    create_temp_directory_if_not_exists()
-    move_files_to_temp()
+    create_janitor_directory_if_not_exists()
+    move_files_to_janitor()
     print("Success")
